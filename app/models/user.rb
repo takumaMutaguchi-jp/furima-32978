171bb786344af4e2f133216ac51dd76e59f4960d
takumaMutaguchi-jp <mutaguchitakuma@gmail.com>
2021-01-04 18:00:36 +0900

    userバリデーションに正規表現を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,18 @@ class User < ApplicationRecord
   # バリデーション
   with_options presence: true do
     validates :nickname
+    validates :birthday
+  end
+  with_options presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/} do
     validates :surname
     validates :given_name
+  end
+  with_options presence: true, format: {with: /\A[ァ-ヶー－]+\z/} do
     validates :surname_kana
     validates :given_name_kana
-    validates :birthday
+  end
+  with_options format: {with: /\A[a-zA-Z0-9]+\z/} do
+    validates :password
+    validates :password_confirmation
   end
 end
