cc1b1cfed1161d05a66912f53557ae1a5bada291
takumaMutaguchi-jp <mutaguchitakuma@gmail.com>
2021-01-06 15:50:23 +0900

    iteｍ - category のアソシエーションを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,10 @@
 class Item < ApplicationRecord
 
   # アソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
   belongs_to :user
   has_one_attached :image
+  
 
 end
