dd0f9e3c5b3fe98a8c8efbc5c69ad9482195f4c2
takumaMutaguchi-jp <mutaguchitakuma@gmail.com>
2021-01-06 17:48:59 +0900

    item - days_to_ship のアソシエーションを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,7 @@ class Item < ApplicationRecord
   belongs_to :item_state
   belongs_to :shipping_burden
   belongs_to :prefecture
+  belongs_to :days_to_ship
   belongs_to :user
   has_one_attached :image
   
