50ed8416310aac5e713a26c04a373682273dfded
takumaMutaguchi-jp <mutaguchitakuma@gmail.com>
2021-01-06 16:34:09 +0900

    item - item_stateのアソシエーションを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   # アソシエーション
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :item_state
   belongs_to :user
   has_one_attached :image
   
--- app/models/item_state.rb
+++ app/models/item_state.rb
@@ -8,4 +8,8 @@ class ItemState < ActiveHash::Base
     { id: 6, name: '傷や汚れあり' },
     { id: 7, name: '全体的に状態が悪い' }
   ]
+
+  # アソシエーション
+  include ActiveHash::Associations
+  has_many :items
 end
