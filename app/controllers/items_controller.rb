0a9806e142ba8804e8c243005d456bfd47b50e4b
takumaMutaguchi-jp <mutaguchitakuma@gmail.com>
2021-01-10 17:39:20 +0900

    destoryアクションに出品者のみ削除できるよう記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,8 +35,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to action: :index
+    if current_user.id == @item.user_id
+      @item.destroy
+      redirect_to action: :index
+    end
   end
 
   private
