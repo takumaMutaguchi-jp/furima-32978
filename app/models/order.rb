68dbea016a3cfe52bb287e8c1f382d9e2b6db7d8
takumaMutaguchi-jp <mutaguchitakuma@gmail.com>
2021-01-11 12:36:39 +0900

    orderモデルのバリデーションを設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,15 @@ class Order < ApplicationRecord
 
   # アソシエーション
   belongs_to :purchase_management
-  
+
+  # バリデーション
+  with_options presence: true do
+    validates :postal_code
+    validates :prefecture_id
+    validates :municipality
+    validates :address
+    validates :phone_number
+    validates :purchase_managemen
+  end
+
 end
