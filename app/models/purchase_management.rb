b5709de6e402ead2992f96bd8b7726cc59bfb5b2
takumaMutaguchi-jp <mutaguchitakuma@gmail.com>
2021-01-11 12:38:41 +0900

    purchase_managementモデルのバリデーションを設定

--- app/models/purchase_management.rb
+++ app/models/purchase_management.rb
@@ -1,7 +1,14 @@
 class PurchaseManagement < ApplicationRecord
-  
+
   # アソシエーション
   belongs_to :user
   belongs_to :item
   has_one :order
+
+  # バリデーション
+  with_options presence: true do
+    validates :user
+    validates :item
+  end
+
 end
