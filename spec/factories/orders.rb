b20e6e77abb1e4ac6af90eb0baeb4ff02207393c
takumaMutaguchi-jp <mutaguchitakuma@gmail.com>
2021-01-13 09:36:06 +0900

    orderモデルのFactoryBotの作成

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :order do
-    
+    postal_code   { 1234567.to_s.insert(3, "-") }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    municipality  { Faker::Address.street_name }
+    address       { Faker::Address.street_address }
+    building_name { Faker::Address.secondary_address }
+    phone_number  { Faker::PhoneNumber.subscriber_number(length: 11) }
+    association   :purchase_management
   end
 end
