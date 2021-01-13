ec54d2dee401192dba6e4feb56e5f3a99f580271
takumaMutaguchi-jp <mutaguchitakuma@gmail.com>
2021-01-13 14:12:30 +0900

    userモデルのFactorybotの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     end
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { '1a' + Faker::Internet.password(min_length: 4) }
     password_confirmation { password }
     surname               { person.last.kanji }
     given_name            { person.first.kanji }
