aa29a6a0cb75e1eecd69ace4e44e472ae4f758a5
takumaMutaguchi-jp <mutaguchitakuma@gmail.com>
2021-01-15 18:02:49 +0900

    バリデーションの記述をリファクタリング

--- app/models/order_purchase.rb
+++ app/models/order_purchase.rb
@@ -5,15 +5,18 @@ class OrderPurchase
 
   # バリデーション
   with_options presence: true do
+    validates :postal_code
+    validates :phone_number
+    validates :prefecture_id
     validates :municipality
     validates :address
     validates :token
     validates :user_id
     validates :item_id
   end
-  validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}+\z/ }
-  validates :phone_number, presence: true, format: { with: /\A\d{11}+\z/ }
-  validates :prefecture_id, presence: true, numericality: { other_than: 1 }
+  validates :postal_code, format: { with: /\A\d{3}-\d{4}+\z/ }
+  validates :phone_number, format: { with: /\A\d{11}+\z/ }
+  validates :prefecture_id, numericality: { other_than: 1 }
 
   def save
     purchase_management = PurchaseManagement.create(user_id: user_id, item_id: item_id)
