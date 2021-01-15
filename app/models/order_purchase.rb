aacb5c8556c7c997a28da6820172edbd9444ff2a
takumaMutaguchi-jp <mutaguchitakuma@gmail.com>
2021-01-15 15:41:24 +0900

    フォームオブジェクトにattr_accessorを追加

---
+++ app/models/order_purchase.rb
@@ -0,0 +1,8 @@
+class OrderPurchase
+
+  include ActiveModel::ActiveModel
+  attr_accessor :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :purchase_management_id, :user_id, :item_id
+
+  
+
+end
