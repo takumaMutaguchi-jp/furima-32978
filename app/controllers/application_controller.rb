6624c0b8d85267bc734a7e731803e3e2302ab132
takumaMutaguchi-jp <mutaguchitakuma@gmail.com>
2021-01-15 20:06:14 +0900

    applicationコントローラの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
